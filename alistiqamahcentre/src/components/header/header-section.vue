3570cf82fa85f371840fc9082b725e409bf74dee
sihamali <siham.s18@hotmail.com>
2023-09-27 22:59:22 -0600

    Logo and menu tile

--- alistiqamahcentre/src/components/header/header-section.vue
+++ alistiqamahcentre/src/components/header/header-section.vue
@@ -2,13 +2,11 @@
     <h2>
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-                    <!-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" /> -->
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">Al Istiqamah Centre
-                        Logo</span>
-                
+                     <img src="\src\assets\Logo1-B&W.png" class="h-28 mr-3" alt="Al Istiqamah Logo" />
+                    <!-- <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">Al Istiqamah Centre</span> -->
+                    <menu-tile />
             </div>
         </nav>
-        <menu-tile />
     </h2>
 </template>
 <script setup lang="ts">
--- alistiqamahcentre/src/components/header/menu-tile.vue
+++ alistiqamahcentre/src/components/header/menu-tile.vue
@@ -1,21 +1,22 @@
 <template>
-    <nav class="bg-gray-50 dark:bg-gray-700">
+    <nav >
         <div class="max-w-screen-xl px-4 py-3 mx-auto">
             <div class="flex items-center">
                 <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
                     <li>
-                        <a href="#" class="text-gray-900 dark:text-white" aria-current="page">Menu item 1</a>
+                        <a href="#" class="text-gray-900 dark:text-white" aria-current="page">About Us</a>
                     </li>
                     <li>
-                        <a href="#" class="text-gray-900 dark:text-white">Menu item 2</a>
+                        <a href="#" class="text-gray-900 dark:text-white">Our Mission</a>
                     </li>
                     <li>
-                        <a href="#" class="text-gray-900 dark:text-white">Menu item 3</a>
+                        <a href="#" class="text-gray-900 dark:text-white">Programs</a>
                     </li>
                     <li>
-                        <a href="#" class="text-gray-900 dark:text-white">Menu item 4</a>
+                        <a href="#" class="text-gray-900 dark:text-white">Contact Us</a>
                     </li>
                 </ul>
+                <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Donate</button>
             </div>
         </div>
     </nav>
